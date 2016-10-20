4251fbce32127f167f53b1c9fbcff14800b56bdc
joaogodinho <joaocfgodinho@gmail.com>
2016-10-20 20:25:19 +0100

    Changed graphs to percentage, added percentage of duplicates.

--- short_analysis/analyze.py
+++ short_analysis/analyze.py
@@ -17,6 +17,17 @@ def loadCSV():
     return df
 
 
+def plotAnalysesByMonth(df, name, title):
+    ticklabels = [item.strftime('%b %Y') for item in df.index]
+    ax = df.plot(kind='bar', stacked=True, figsize=(20, 10), alpha=0.6)
+    ax.xaxis.set_major_formatter(mticker.FixedFormatter(ticklabels))
+    plt.title(title)
+    plt.ylabel('Percent (%)')
+    plt.savefig('{0}.png'.format(name), bbox_inches='tight')
+    plt.show()
+
+
+
 if __name__ == '__main__':
     df = loadCSV()
 
@@ -24,20 +35,32 @@ if __name__ == '__main__':
     crit_malware = df.antivirus.map(lambda x: x != 'n/a' and not x.startswith('0/'))
     crit_na = df.antivirus.map(lambda x: x == 'n/a')
     crit_notmalware = df.antivirus.map(lambda x: x.startswith('0/'))
+    crit_duplicated = df.md5.duplicated()
 
     # Split analyses by malware, no scan at time or not malware
-    anal_month_malware = df.md5[crit_malware].resample('M').count()
-    anal_month_malware = anal_month_malware.rename('Malware (1+ detections)')
-    anal_month_na = df.md5[crit_na].resample('M').count()
-    anal_month_na = anal_month_na.rename('Not scanned')
-    anal_month_notmalware = df.md5[crit_notmalware].resample('M').count()
-    anal_month_notmalware = anal_month_notmalware.rename('Not malware/Unknown at time')
-    # Group results
-    anal_month = pd.concat([anal_month_malware, anal_month_notmalware, anal_month_na], axis=1)
+    anal_malware = df.md5[crit_malware].resample('M').count()
+    anal_malware = anal_malware.rename('Malware (1+ detections)')
+    anal_na = df.md5[crit_na].resample('M').count()
+    anal_na = anal_na.rename('Not scanned')
+    anal_notmalware = df.md5[crit_notmalware].resample('M').count()
+    anal_notmalware = anal_notmalware.rename('Not malware/Unknown at time')
 
-    ticklabels = [item.strftime('%b %Y') for item in anal_month.index]
-    ax = anal_month.plot(kind='bar', figsize=(20, 10))
-    ax.xaxis.set_major_formatter(mticker.FixedFormatter(ticklabels))
-    plt.ylabel('#Analyses')
-    plt.savefig('months.png', bbox_inches='tight')
-    plt.show()
+    # Split analyses by md5 duplicates
+    anal_unique = df.drop_duplicates(subset='md5').md5.resample('M').count()
+    anal_unique = anal_unique.rename('Unique MD5')
+    anal_dups = df.md5[crit_duplicated].resample('M').count()
+    anal_dups = anal_dups.rename('Not unique MD5')
+
+
+    # Group results
+    anal_classification = pd.concat([anal_malware, anal_notmalware, anal_na], axis=1)
+    anal_classification = anal_classification.divide(anal_classification.sum(axis=1), axis=0)
+    anal_unique_duplicates = pd.concat([anal_unique, anal_dups], axis=1)
+    anal_unique_duplicates = anal_unique_duplicates.divide(anal_unique_duplicates.sum(axis=1), axis=0)
+
+    # Plot
+    plotAnalysesByMonth(anal_classification, 'classifcation', 'Analyses classification')
+    plotAnalysesByMonth(anal_unique_duplicates, 'unique_dups', 'Duplicate samples')
+
+
+
