094199888f7ec256155a7c03d9a8dd261a475159
joaogodinho <joaocfgodinho@gmail.com>
2017-07-04 16:27:24 +0100

    Fixed bugs.

--- create_csv.py
+++ create_csv.py
@@ -9,13 +9,13 @@ import pandas as pd
 import numpy as np
 
 
-def main(path):
+def main(path, outpath):
     path = path + '/' if path[-1] != '/' else path
+    outpath = outpath + '/' if outpath[-1] != '/' else outpath
     (_, _, reports) = next(walk(path))
     BATCH_SIZE = 1000
     counter = 0
     batches = [reports[i:i+BATCH_SIZE] for i in range(0, len(reports), BATCH_SIZE)]
-    out_file = 'data/peimphash.csv.gz'
     for idx, batch in enumerate(batches):
         jobs = group([tasks.extract_content.s('extract_peimphash', report, path) for report in batch])
         result = jobs.apply_async()
@@ -24,7 +24,7 @@ def main(path):
         frame = pd.DataFrame(data=list(arr[:,1]), index=arr[:,0])
         frame.index.name = 'link'
         frame.columns = ['pe_imphash']
-        frame.to_csv(path_or_buf='data/checkpoints/pe32_peimphash{}.csv.gz'.format(idx + 1), compression='gzip')
+        frame.to_csv(path_or_buf=outpath + 'pe32_peimphash{}.csv.gz'.format(idx + 1), compression='gzip')
 
 
 if __name__ == '__main__':
--- tasks.py
+++ tasks.py
@@ -9,15 +9,17 @@ import lib.report_parsers as jcfg_report_parsers
 import json
 
 
-# app = Celery('tasks', backend='rpc://', broker='pyamqp://jcfg:jcfg@localhost/thesis')
-app = Celery('tasks', backend='rpc://')
+app = Celery('tasks', backend='rpc://', broker='pyamqp://jcfg:jcfg@localhost/thesis')
 
 
 @app.task
 def extract_content(key, report, path):
     with gzip.open(path + report) as gz_file:
         content = json.loads(gz_file.read().decode('utf8'))
-        return (report, content[key])
+        if key in content:
+            return (report, content[key])
+        else:
+            return (report, None)
 
 
 @app.task
