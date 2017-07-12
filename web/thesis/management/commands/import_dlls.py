77500dde4457c6b885ef0642efa1a74a4979bde8
joaogodinho <joaocfgodinho@gmail.com>
2017-07-12 10:55:07 +0100

    Using gevent to speed up insertion.

--- web/thesis/management/commands/import_dlls.py
+++ web/thesis/management/commands/import_dlls.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from thesis.models import Report, DLL, UsesDLL
 import pandas as pd
+import gevent
+from gevent import monkey; monkey.patch_all()
 """
     Imports used DLLs into the DB
 """
@@ -9,6 +11,12 @@ import pandas as pd
 class Command(BaseCommand):
     help = 'Import CSV with DLLs into the DB'
 
+    def create_uses_dll(self, report, dlls):
+        r = Report.objects.get(link=report)
+        for d in dlls:
+            dll = DLL.objects.get(name=d)
+            UsesDLL.objects.update_or_create(report=r, dll=dll)
+
     def add_arguments(self, parser):
         parser.add_argument('file', type=str)
         parser.add_argument('dlls', type=str)
@@ -16,20 +24,21 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         file_name = options['file']
         dlls_name = options['dlls']
-        # with open(dlls_name, 'r') as file:
-        #     dlls = list(map(lambda x: x.replace('.dll', ''), map(str.strip, file.readlines())))
-        # for dll in dlls:
-        #     DLL.objects.create(name=dll)
+        with open(dlls_name, 'r') as file:
+            dlls = list(map(lambda x: x.replace('.dll', ''), map(str.strip, file.readlines())))
+        for dll in dlls:
+            DLL.objects.create(name=dll)
 
         reports_frame = pd.read_csv(file_name)
         reports_frame = reports_frame.set_index('link')
 
+        jobs = []
         for report in reports_frame.iterrows():
-            r = Report.objects.get(link=report[0])
             dlls = map(lambda x: x.strip(), report[1].dlls.split(';'))
             dlls = filter(lambda x: x.endswith('.dll'), dlls)
             dlls = map(lambda x: x.replace('.dll', ''), dlls)
-            for d in dlls:
-                print(d)
-                dll = DLL.objects.get(name=d)
-                UsesDLL.objects.update_or_create(report=r, dll=dll)
+            jobs.append(gevent.spawn(self.create_uses_dll, report[0], dlls))
+
+        # Done spawning, wait
+        print('Waiting for jobs...')
+        gevent.joinall(jobs)
