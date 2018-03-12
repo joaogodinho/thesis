435d25254e83513c6e026312b4e52eff622b1300
joaogodinho <joaocfgodinho@gmail.com>
2018-03-12 16:19:55 +0000

    Added task to extract dynamic api calls.

--- extract/tasks.py
+++ extract/tasks.py
@@ -134,3 +134,34 @@ def behavior_categories(report):
             for call in proc['calls']:
                 result[call['category']] = result.setdefault(call['category'], 0) + 1
     return result
+
+
+@app.task
+def behavior_func_calls(report):
+    STR0 = '<div id="graph_process_details">'
+    STR1 = '<script type="text/javascript">'
+    STR2 = 'var graph_raw_data = '
+    STR3 = '</script>'
+    with gzip.open(report) as gzip_file:
+        content = gzip_file.read().decode('utf8')
+
+    result = dict()
+    result['link'] = report
+
+    # Use specific strings to find the dynamic calls,
+    # as sometimes the file is too big for etree
+    content = content[content.find(STR0):]
+    content = content[content.find(STR1) + len(STR1):].replace(STR2, '')
+    content = content[:content.find(STR3)]
+    content = content.strip()[:-1] # -1 removes ending ';'
+    if content is not None and len(content) != 0:
+        try:
+            processes = json.loads(content)
+        except:
+            print(report)
+            raise
+        # Get only the categories
+        for proc in processes:
+            for call in proc['calls']:
+                result[call['api']] = result.setdefault(call['api'], 0) + 1
+    return result
